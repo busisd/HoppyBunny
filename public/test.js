30c6b468ef5295bb3590327b6add29c4cbf847b6
busisd <busisd@carleton.edu>
2020-05-09 19:03:20 -0400

    Now includes a smartly draggable bunny!

--- public/test.js
+++ public/test.js
@@ -9,7 +9,10 @@
 // ********************
 
 const app = new PIXI.Application({
-  width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+  width: 600,
+  height: 600,
+  backgroundColor: 0x1099bb,
+  resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(app.view);
 
@@ -18,14 +21,15 @@ const container = new PIXI.Container();
 app.stage.addChild(container);
 
 // Create a new texture
-const texture = PIXI.Texture.from('assets/bunny.png');
+const texture = PIXI.Texture.from("assets/bunny.png");
 
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
+// Create a nxn grid of bunnies
+const numBunnies = 8;
+for (let i = 0; i < numBunnies ** 2; i++) {
   const bunny = new PIXI.Sprite(texture);
   bunny.anchor.set(0.5);
-  bunny.x = (i % 5) * 64;
-  bunny.y = Math.floor(i / 5) * 64;
+  bunny.x = (i % numBunnies) * 64;
+  bunny.y = Math.floor(i / numBunnies) * 64;
   container.addChild(bunny);
 }
 
@@ -41,5 +45,48 @@ container.pivot.y = container.height / 2;
 app.ticker.add((delta) => {
   // rotate the container!
   // use delta to create frame-independent transform
-  container.rotation -= 0.01 * delta;
+  container.rotation += 0.01 * delta;
 });
+
+const container2 = new PIXI.Container();
+app.stage.addChild(container2);
+
+const bun = new PIXI.Sprite(texture);
+bun.x = app.screen.width - 64;
+bun.y = 0;
+bun.interactive = true;
+bun.buttonMode = true;
+bun
+  .on(
+    "pointerdown",
+    ({
+      data: {
+        button,
+        global: { x, y },
+      },
+    }) => {
+      if (button == 0) {
+        bun.dragXOffset = bun.x - x;
+        bun.dragYOffset = bun.y - y;
+        bun.dragEnabled = true; 
+      }
+    }
+  )
+  .on("pointermove", ({
+    data: {
+      global: { x, y }
+    },
+  }) => {
+    if (bun.dragEnabled) {
+      bun.x = x + bun.dragXOffset;
+      bun.y = y + bun.dragYOffset;
+    }
+  })
+  .on("pointerup", () => bun.dragEnabled = false)
+  .on("pointerupoutside", () => bun.dragEnabled = false);
+container2.addChild(bun);
+
+bun.dragEnabled = false;
+bun.dragXOffset = 0;
+bun.dragYOffset = 0;
+
