d353dedb456f2991ed1a755a75feddadbd8df00a
busisd <busisd@carleton.edu>
2020-05-11 00:24:59 -0400

    Added two more URLs to display an old test, and a fixed-size version of the game

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const port = 3000;
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendfile('public/index.html'));
+app.get('/', (req, res) => res.sendFile('public/index.html', { root: __dirname }));
+app.get('/test', (req, res) => res.sendFile('public/test1.html', { root: __dirname }));
+app.get('/fixed', (req, res) => res.sendFile('public/fixed_size_game.html', { root: __dirname }));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
